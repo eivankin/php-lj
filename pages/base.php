12ca011a0419974dcf2b025d2d589168fde101c2
eivankin <>
2022-05-01 00:37:26 +0300

    design WIP

--- pages/base.php
+++ pages/base.php
@@ -8,21 +8,23 @@
     </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
 <header>
-    <h1>Блог</h1>
+    <img src="/assets/logo.svg" width="40" height="40">
     <nav>
-        <ul>
+        <ul id="menu">
             <li><a href="/">Главная</a></li>
             <li><a href="/users">Пользователи</a></li>
             <li><a href="/entries">Публикации</a></li>
             <?php
             if (isset($_SESSION['user_id'])) {
-                echo '<li><a href="/account">Личный кабинет</a></li>';
-                echo '<li><a href="/logout">Выйти</a></li>';
+                echo '<li><a href="/account">Личный кабинет</a></li>
+                <li><a href="/logout">Выйти</a></li>';
             } else {
-                echo '<li><a href="/login">Войти</a></li>';
+                echo '<li><a href="/login">Войти</a></li>
+                <li><a href="/register">Зарегистрироваться</a></li>';
             }
             ?>
         </ul>
