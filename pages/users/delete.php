f4c0ac77280e29639361256c88a5cf0f7353c3ca
eivankin <>
2022-05-02 02:26:50 +0300

    restrict self-deletion

--- pages/users/delete.php
+++ pages/users/delete.php
@@ -7,16 +7,11 @@ $title = 'Удаление пользователя';
 if (!isset($id))
     $id = null;
 handle_page_with_id($id, 'users', '/delete');
-$is_the_same_user = $_SESSION['user_id'] == $id;
 
-if (!$is_the_same_user && !has_permission($_SESSION['user_id'], ADMIN)) {
+if ($_SESSION['user_id'] == $id || !has_permission($_SESSION['user_id'], ADMIN)) {
     $message = 'У вас нет прав на удаление этого пользователя';
 } else {
     if (delete_user($id)) {
-        if ($is_the_same_user) {
-            header('Location: /logout');
-            exit();
-        }
         $message = 'Пользователь успешно удалён';
     } else {
         $message = 'Не удалось удалить пользователя';
