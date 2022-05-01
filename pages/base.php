66d0613a739fe744f20b41406d49b61703406174
eivankin <>
2022-05-01 14:35:21 +0300

    fix nav design

--- pages/base.php
+++ pages/base.php
@@ -11,20 +11,23 @@
     <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
-<header>
-    <img src="/assets/logo.svg" width="40" height="40" alt="Логотип">
+<header id="top">
     <nav>
-        <ul id="menu">
+        <ul class="menu">
+            <li><img src="/assets/logo.svg" width="40" height="40" alt="Логотип"></li>
             <li><a class="white" href="/">Главная</a></li>
             <li><a class="white" href="/users">Пользователи</a></li>
             <li><a class="white" href="/entries">Публикации</a></li>
+        </ul>
+    </nav>
+    <nav>
+        <ul class="menu">
             <?php
             if (isset($_SESSION['user_id'])) {
                 echo '<li><a class="white" href="/account">Личный кабинет</a></li>
                 <li><a class="white" href="/logout">Выйти</a></li>';
             } else {
-                echo '<li><a class="white" href="/login">Войти</a></li>
-                <li><a class="white" href="/register">Зарегистрироваться</a></li>';
+                echo '<li><a class="white" href="/login">Войти</a></li>';
             }
             ?>
         </ul>
@@ -40,7 +43,7 @@
     ?>
 </main>
 <footer>
-    <p><a class="white" href="#menu">↑ Наверх</a></p>
+    <p><a class="white" href="#top">↑ Наверх</a></p>
     <p>Связаться с администратором: <a class="white" href="mailto:webmaster@<?php echo $_SERVER['HTTP_HOST'] ?>">
             webmaster@<?php echo $_SERVER['HTTP_HOST'] ?></a></p>
     <hr>
