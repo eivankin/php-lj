469033c4d48ceb29335e445ef5dbe65d470cfc7a
eivankin <>
2022-05-08 12:11:45 +0300

    add more info to entry card

--- pages/entries/util.php
+++ pages/entries/util.php
@@ -1,8 +1,15 @@
 <?php
 require_once 'db/user/util.php';
+require_once 'db/category/util.php';
+require_once 'db/tag/util.php';
 
-function make_entry_card(array $entry): string {
+function make_entry_card(array $entry): string
+{
     $author = get_user($entry['author_id']);
+    $category = get_category($entry['category_id']);
+    $tags = join(' | ', array_map(function ($t) {
+        return "<a href='/entries/?tags[]={$t['id']}'>{$t['name']}</a>";
+    }, get_entry_tags($entry['id'])));
     $views = '';
     if (isset($entry['views_count']))
         $views = "<p>Просмотры: {$entry['views_count']}</p>";
@@ -13,6 +20,8 @@ function make_entry_card(array $entry): string {
         <div class='card-body'>
             <p>Автор: <a href='/users/{$author['id']}'>{$author['username']}</a></p>
             <p>Опубликовано: {$entry['published']}</p>
+            <p>Категория: <a href='/entries/?category={$category['id']}'>{$category['name']}</a></p>
+            <p>Теги: {$tags}</p>
             {$views}
             <p><a href='/entries/{$entry['id']}'><button>Читать</button></a></p>
         </div>
