e3fe29703668a74ac1fc21850c7b5931f399e387
eivankin <>
2022-10-08 17:00:13 +0300

    upd style & minor fixes

--- pages/index.php
+++ pages/index.php
@@ -7,7 +7,14 @@ require_once 'pages/entries/util.php';
  */
 
 $title = 'Главная страница';
-$content = '';
+$content = '<h1>Добро пожаловать!</h1>
+	<div style="display: flex; justify-content: space-evenly;align-items: center;text-align: center;">
+	<div>
+	<h3>Есть, чем поделиться?</h3><p><a class="white" href="/entries/new">
+		<button>Опубликуйте свой материал!</button>
+	</a></p></div>
+	<div><h3>Хочется почитать что-нибудь интересное?</h3><p>Ознакомьтесь с подборками, представленными ниже!</p>
+	</div></div>';
 
 if (isset($_SESSION['user_id'])) {
     $content .= '<h2>Ваши подписки</h2>';
@@ -19,7 +26,12 @@ if (isset($_SESSION['user_id'])) {
     foreach ($subscription_entries as $entry)
         $content .= make_entry_card($entry);
 
-    $content .= '</div>';
+    $content .= '</div>
+<div style="display: flex; justify-content: center; margin-top: 20px;">
+	<a href="/entries/">
+		<button style="text-transform: uppercase">Больше публикаций</button>
+	</a>
+</div>';
 }
 
 $content .= '<h2>Популярные публикации</h2>';
@@ -29,5 +41,10 @@ if (count($popular) < 1)
     $content .= '<p>Новых публикаций нет.</p>';
 foreach ($popular as $entry)
     $content .= make_entry_card($entry);
-$content .= '</div>';
+$content .= '</div>
+<div style="display: flex; justify-content: center; margin-top: 20px;">
+	<a href="/entries/">
+		<button style="text-transform: uppercase">Больше публикаций</button>
+	</a>
+	</div>';
 require_once 'pages/base.php';
